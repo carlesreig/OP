e89417773bdf26041a3a6c16f3a27d88e5556796
Carles Reig <carlesreig@gmail.com>
2016-01-09 09:46:40 +0100

    Create fs.sh

---
+++ fs.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# comprovar File System (FS)
+# 2016/01/09 @ 09:44h
+echo "A quin servidor vols accedir?"
+read HOST
+echo "escriu la ruta absoluta del FS a revisar"
+read FS
+PASS=$(cat pwd.txt)
+
+clear
+
+	echo ""
+	echo "========================================================================="
+	echo ""
+	echo "  Filesystem de $HOST"
+	echo ""
+	echo "========================================================================="
+	echo ""
+	VAR=$(expect -c " 
+	spawn ssh $HOST /bin/df -k $FS;
+	expect \"yes/no\" { 
+	    send -- \"yes\r\"
+	    expect \"*?assword\" { send -- \"$PASS\r\" }
+	    } \"*?assword\" { send -- \"$PASS\r\" }
+	send -- \"\r\"
+	expect eof
+        ")
+	echo "$VAR"|tail -4
+
+        echo ""
+        echo "===================================Archiu de major ocupacio ======================================"
+        echo ""
+
+        VAR1=$(expect -c "
+        spawn ssh $HOST find $FS 2>/dev/null -xdev -type f -ls;
+        expect \"yes/no\" {
+            send -- \"yes\r\"
+            expect \"*?assword\" { send -- \"$PASS\r\" }
+            } \"*?assword\" { send -- \"$PASS\r\" }
+        send -- \"\r\"
+        expect eof
+        ")
+        echo "$VAR1"|awk '{print $7"\t"$5"\t"$11}'|sort -rn|head -5|more
+
+        
+	echo ""
+        echo "=================================================================================================="
+        echo ""

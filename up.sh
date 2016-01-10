314869d0e2b76ef404696db5f1bec964bd0e6d96
Carles Reig <carlesreig@gmail.com>
2016-01-10 09:30:49 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# 2016/01/09
+# 2016/01/10
 # script per a comprovar si un servidor esta ON, uptime amb el temps que porta funcionant
+func_debug
 # cridem a la funcio "func_servidor" per a connectar al servidor
-func_servidor
+#func_servidor
 
 clear
 	echo "  Uptime a $HOST"
 	echo ""
 	echo "========================================================================="
 	echo ""
+	# després de expect posem les comandes que volem executar en el servidor
 	VAR=$(expect -c " 
 		spawn ssh $HOST echo \; uptime && uname -a && date;
 	" func_pswd_expect ")"

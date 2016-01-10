c23f488113a89d5d32fc4d905adde544ffde588b
Carles Reig <carlesreig@gmail.com>
2016-01-10 13:07:20 +0100

    Update up.sh

--- up.sh
+++ up.sh
@@ -5,9 +5,20 @@
 # cridem a la funcio "func_servidor" per a connectar al servidor
 func_servidor
 
+# spawn ssh -o "StrictHostKeyChecking no" "$user\@ip"
+# consulta amb les comandes que volem executar en el servidor $HOST
+$CONSULTA="spawn ssh -o "StrictHostKeyChecking no" $HOST echo \; uptime && uname -a && date;"
+
 clear
 echo "Uptime a $HOST"
 echo "========================================================================="
-# consulta amb les comandes que volem executar en el servidor $HOST
-$CONSULTA="spawn ssh $HOST echo \; uptime && uname -a && date;"
-func_pswd_expect($CONSULTA)
+	VAR=$(expect -c " 
+	$CONSULTA
+	expect \"yes/no\" { 
+	    send -- \"yes\r\"
+	    expect \"*?assword\" { send -- \"$PASS\r\" }
+	    } \"*?assword\" { send -- \"$PASS\r\" }
+	send -- \"\r\"
+	expect eof
+        ")
+	echo "$VAR"

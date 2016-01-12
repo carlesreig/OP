1aaaf0b00c93c594a059b2e1b4a6143ce97a5a65
Carles Reig <carlesreig@gmail.com>
2016-01-12 08:19:22 +0100

    Update menu.sh
    he aplicat tots els canvis suggerits en el menu2.sh. Podem treballar directament sobre aquest menú, si anem comentant tots els canvis tampoc hi ha gaire problema, a part que sempre queda l'historial per a revisar i arxius locals (copies), per no crear 50 scripts diferents del mateix ;-)

--- menu.sh
+++ menu.sh
@@ -1,59 +1,86 @@
 #!/bin/sh
-# 2016/01/10
 # Scripts creat per Ricard Rabert Brugue i modificat per Carles Reig @ 2016
+# Menu principal on declarem les funcions, variables globals i accedim a les diferents shells/opcions
+clear
+
+# definicio de colors
+NORMAL=`echo "\033[m"`
+MENU=`echo "\033[36m"` #Blau cel
+NUMBER=`echo "\033[33m"` #groc
+FGRED=`echo "\033[41m"`
+RED_TEXT=`echo "\033[31m"`
+ENTER_LINE=`echo "\033[33m"`
+COLOR='\033[01;31m' # Negretas en vermell
+RESET='\033[00;00m' # normal blanca
+
+# declarem variable on desar la contrasenya d'usuari per accedir al servidor
+# demana el pwd de unix al usuari en cada execucio del script menu
+echo -e "${COLOR}Introdueix el PWD de usuari UNIX"
+read -s PASS
+# tambe podem desar la contrasenya dins un fitxer en el servidor
+# PASS=$(cat pwd.txt)
+export PASS
+
+clear
 _menu() {
-	# declarem els colors
-	NORMAL=`echo "\033[m"`
-	MENU=`echo "\033[36m"` # Blau cel
-	NUMBER=`echo "\033[33m"` # groc
-	FGRED=`echo "\033[41m"`
-	RED_TEXT=`echo "\033[31m"`
-	ENTER_LINE=`echo "\033[33m"`
-	# mostrem el menu principal
-	# PENDENT crear un submenú per a cada secció 1x (UNIX), 2x (FS), 3x (BBDD)...
-	echo -e "${MENU}*********************************************${NORMAL}"
-	echo -e "${MENU}****************1x-UNIX*************************${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 10)${MENU} not running perfalarm ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 11)${MENU} not running standard ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 1)${MENU} Too many instances X ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 3)${MENU} ONE least ITO ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 5)${MENU} NTP offset ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 6)${MENU} not running ntpd verificacio ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 7)${MENU} not running ntpd arranc ${NORMAL}"
-	echo -e "${MENU}*****************2x-FS**************************${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 2)${MENU} FS i arxius major ocupacio ${NORMAL}"
-	echo -e "${MENU}****************3x-BBDD*************************${NORMAL}"
-	echo -e "${MENU}**${NUMBER} XX)${MENU} comprovar si BBDD està aixecada ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} XX)${MENU} comprovar si instància BBDD està OPEN ${NORMAL}"
-	echo -e "${MENU}**************4x - RENDIMENT**********************${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 8)${MENU} CPU & Process ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 9)${MENU} Uptime ${NORMAL}"
-	echo -e "${MENU}**${NUMBER} 4)${MENU} Mem Swap & FreeE ${NORMAL}"
-	echo -e "${MENU}*********************************************${NORMAL}"
-	echo -e "${MENU}**${NUMBER} s)${MENU} Exit ${NORMAL}"
-	echo -e "${MENU}*********************************************${NORMAL}"
-	echo -e "${ENTER_LINE}Selecciona la opcio i presiona enter o ${RED_TEXT}presiona S per sortir. ${NORMAL}"
+    echo -e "${MENU}****************************************************${NORMAL}"
+    echo -e "${MENU}******************** Unix **************************${NORMAL}"
+#    echo -e "${MENU}**"
+    echo -e "${MENU}**${NUMBER} 11)${MENU} Too many instances X ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 12)${MENU} ONE least ITO ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 13)${MENU} NTP offset ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 14)${MENU} not running ntpd verificacio ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 15)${MENU} not running ntpd ${RED_TEXT} arranc (actualment no tenim permis) ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 16)${MENU} not running perfalarm ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 17)${MENU} not running standard ${NORMAL}"
+#   echo -e "${MENU}**"
+#   echo -e "${MENU}**${NUMBER} 999)${MENU} evweb eventviewer ${NORMAL}" en desenvolupament
+#    echo -e "${MENU}****************************************************${NORMAL}"
+    echo -e "${MENU}******************* Monitoritzacio *****************${NORMAL}"
+#    echo -e "${MENU}**"
+    echo -e "${MENU}**${NUMBER} 21)${MENU} SWAPINFO&FREE ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 22)${MENU} UPTIME ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 23)${MENU} CPU & PROCESS ${NORMAL}"
+#    echo -e "${MENU}**"
+#    echo -e "${MENU}****************************************************${NORMAL}"
+    echo -e "${MENU}******************* FileSystems ********************${NORMAL}"
+#    echo -e "${MENU}**"
+    echo -e "${MENU}**${NUMBER} 31)${MENU} FS i archius major ocupacio ${NORMAL}"
+#    echo -e "${MENU}**"
+    echo -e "${MENU}****************************************************${NORMAL}"
+    echo -e "${MENU}**${NUMBER} s)${MENU} Exit ${NORMAL}"
+    echo -e "${MENU}****************************************************${NORMAL}"
+    echo -e "${MENU}****************************************************${NORMAL}"
+    echo -e "${ENTER_LINE}Selecciona la opcio i presiona enter o ${RED_TEXT}presiona s enter per sortir. ${NORMAL}"
 }
 
 # func que detecta opció escollida en el menu anterior
 function option_picked() {
-	# Negretes en vermell
-	COLOR='\033[01;31m'
-	# normal blanca
-	RESET='\033[00;00m' 
 	MESSAGE=${@:-"${RESET}Error: No message passed"}
 	echo -e "${COLOR}${MESSAGE}${RESET}"
 }
 
-# funció per demanar nom del servidor i variable on desar la contrasenya
+# funció per demanar nom del servidor (host)
 function func_servidor {
 	echo "A quin servidor vols accedir?"
 	read HOST
-	# declarem variable on desar la contrasenya d'usuari per accedir al servidor
-	PASS=$(cat pwd.txt)
 }
 # exportar funcio com a shell (i no només com a variable), ara ja es pot utilitzar en altres scripts cridant la func_*
 export -f func_servidor
+
+#pregunta per la instancia a verificar, per exemple en not running standard
+function func_instancia {
+        echo "Quina instancia vols verificar?"
+        read INSTANCIA
+}
+export -f func_instancia
+
+#pregunta per el filesystem a verificar
+function func_FS {
+        echo "Escriu el FS a revisar"
+        read FS
+}
+export -f func_FS
 
 # opcio per defecte -> cap
 opc="0"
@@ -62,79 +89,95 @@ opc="0"
 # bucle mentres la opcio indicada sigui diferent de s (sortir)
 until [ "$opc" == "s" ]; do
     case $opc in
-    1)
-	  option_picked "Opcio $opc seleccionada";
+
+    11)
+          option_picked "Opcio $opc seleccionada";
         ./too_many_instances.sh;
     _menu
     ;;
-		
-    2)
-	  option_picked "Opcio $opc seleccionada";
-	    ./fs.sh;
+
+    12)
+      option_picked "Opcio $opc seleccionada";
+      ./ONE_least_ITO.sh;
     _menu
     ;;
-	
-	3)
-	  option_picked "Opcio $opc seleccionada";
-        ./ONE_least_ITO.sh;
-    _menu
-    ;;
-	
-	4)
-	  option_picked "Opcio $opc seleccionada";
-	    ./free.sh;
-	_menu
-	;;
-	
-    5)
-	  option_picked "Opcio $opc seleccionada";
+
+    13)
+          option_picked "Opcio $opc seleccionada";
         ./NTP_offset.sh;
     _menu
     ;;
-	
-    6)
-	  option_picked "Opcio $opc seleccionada";
-	    ./ntpd_not_running_verificacio_no_arranc.sh;
-    _menu
-    ;;
-	
-    7)
-	  option_picked "Opcio $opc seleccionada";
-	    ./ntpd_not_running_arranc.sh;
-    _menu ;;
 
-    8)
-      option_picked "Opcio $opc seleccionada";
-        ./top.sh;
+    14)
+          option_picked "Opcio $opc seleccionada";
+            ./ntpd_not_running_verificacio_no_arranc.sh;
     _menu
     ;;
-    9)
-      option_picked "Opcio $opc seleccionada";
-        ./up.sh;
+
+    15)
+          option_picked "Opcio $opc seleccionada";
+            ./ntpd_not_running_arranc.sh;
     _menu
     ;;
-    10)
+
+    16)
        option_picked "Opcio $opc seleccionada";
          ./perfalarm.sh;
     _menu
     ;;
-	
-    11)
+
+    17)
        option_picked "Opcio $opc seleccionada";
          echo "tambe serveix per a les syslogd, xntp, coda, xinetd";
          ./not_running_standard.sh;
     _menu
     ;;
-	
+
+    21)
+      option_picked "Opcio $opc seleccionada";
+      ./free.sh;
+    _menu
+    ;;
+
+    22)
+      option_picked "Opcio $opc seleccionada";
+        ./up.sh;
+    _menu
+    ;;
+
+    23)
+      option_picked "Opcio $opc seleccionada";
+        ./top.sh;
+    _menu
+    ;;
+
+    31)
+       option_picked "Opcio $opc seleccionada";
+       ./fs.sh;
+    _menu
+    ;;
+
+    999)
+       option_picked "Opcio $opc seleccionada";
+         echo "torna el resultat que es demana per a UNIX";
+         ./evwebX.sh;
+    _menu
+    ;;
+
+    111)
+       option_picked "Opcio $opc seleccionada";
+        clear
+        echo "Scripts creat per Ricard Rabert Brugue, colaborador Carles Reig";
+    _menu
+    ;;
     s)
-	exit
-	;;
-	
+        exit
+        ;;
     *)
-# Si no es cap de les anteriors.
+# Si no es ninguna de les anteriors.
     clear
     _menu
     ;;
-	esac
-	read opc
+esac
+  read opc
 done

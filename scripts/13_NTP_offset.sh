591fa8e101dcdcb1bc8c2fe744fd95e073681e87
Carles Reig <carlesreig@gmail.com>
2016-01-30 06:41:37 +0100

    Update 13_NTP_offset.sh
    modificar $COMANDA

--- scripts/13_NTP_offset.sh
+++ scripts/13_NTP_offset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# 2016/01/10
 # script per a comprovacions amb el servidor NTP
 # cridem a la funcio "func_servidor" per a connectar al servidor
 func_servidor
-COMANDA=$(echo \; tail /var/opt/OV/log/OpC/ntp_mon.log && /usr/sbin/ntpq -p)
+# fem la comanda per a tractar la incidencia
+COMANDA=$(tail /var/opt/OV/log/OpC/ntp_mon.log && /usr/sbin/ntpq -p)
 clear
 	echo "  NTP offset a $HOST"
 	echo "========================================================================="

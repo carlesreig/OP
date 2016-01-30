996965a7e30f962a3276746c439777df781c4b49
Carles Reig <carlesreig@gmail.com>
2016-01-30 06:57:52 +0100

    Update 13_NTP_offset.sh
    modificar línia comanda incidencia

--- scripts/13_NTP_offset.sh
+++ scripts/13_NTP_offset.sh
@@ -2,14 +2,12 @@
 # script per a comprovacions amb el servidor NTP
 # cridem a la funcio "func_servidor" per a connectar al servidor
 func_servidor
-# fem la comanda per a tractar la incidencia
-COMANDA=$(tail /var/opt/OV/log/OpC/ntp_mon.log && /usr/sbin/ntpq -p)
 clear
 	echo "  NTP offset a $HOST"
 	echo "========================================================================="
 	echo ""
 	VAR=$(expect -c " 
-	spawn ssh $HOST $COMANDA
+	spawn ssh $HOST tail /var/opt/OV/log/OpC/ntp_mon.log && /usr/sbin/ntpq -p;
 	expect \"yes/no\" { 
 	    send -- \"yes\r\"
 	    expect \"*?assword\" { send -- \"$PASS\r\" }
